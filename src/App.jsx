25764fa1723a6912d0ca4515ebb2972d175bcd3a
mdshahadad <social.shahadad@gmail.com>
2024-04-19 15:17:44 +0600

    Navbar with banner and footer design complted with daisyui and tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ function App() {
 
   return (
     <div>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
     </div>
   )
 }
